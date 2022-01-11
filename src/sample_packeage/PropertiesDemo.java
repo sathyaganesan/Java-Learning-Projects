f7cdb4054bb0752d7a2c503a6293ee9d45e17878
Ganesan <sathya.ganesan@accenture.com>
2022-01-11 12:43:27 -0500

    Added GenericsDemo Class

--- src/sample_packeage/PropertiesDemo.java
+++ src/sample_packeage/PropertiesDemo.java
@@ -1,10 +1,24 @@
 package sample_packeage;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertiesDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Properties p = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\sathya.ganesan\\eclipse-workspace\\Sample_Code_Java\\src\\sample_packeage\\test.properties");
+		p.load(fis);
+		System.out.println("Data in File " +p);
+		
+		String ss = p.getProperty("dosai");
+		System.out.println("Get Property " + ss);
+		
+		p.setProperty("poori", "17");
+		FileOutputStream fos = new FileOutputStream("C:\\Users\\sathya.ganesan\\eclipse-workspace\\Sample_Code_Java\\src\\sample_packeage\\test.properties");
+		p.store(fos, "Updated Price");
 	}
-
 }
